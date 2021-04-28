c595da286001a31ee50cc200f239504f1f760fb7
ListerineBoi <adrianstefano99@gmail.com>
2021-04-28 22:36:27 +0700

    UAT & External Data Source

---
+++ Test Suites/Uji Coba Login Dengan Data Tes.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Uji Coba Login Dengan Data Tes</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6cff8a5f-2242-460d-80f5-77814a047198</testSuiteGuid>
+   <testCaseLink>
+      <guid>9e509fdd-26ac-4e1d-8170-fe04286614b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login -Variable</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>856bb139-7b08-4f9d-8b74-fe16653bae14</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Login</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>856bb139-7b08-4f9d-8b74-fe16653bae14</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>ee83959b-3956-4996-b98d-103290a5b221</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>856bb139-7b08-4f9d-8b74-fe16653bae14</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>dcf0556f-2e67-494d-aada-c8ca92ce05a5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a3abd821-785c-4fe7-b17d-695a2ddd0c5a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
